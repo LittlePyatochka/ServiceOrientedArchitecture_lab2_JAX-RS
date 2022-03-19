1ee651fddf479b29093daa67c2445b881ad64d82
LittlePyatochka <lexica2000@mail.ru>
2022-03-19 04:02:25 +0300

    first commit

--- web-app/src/main/java/kamysh/exceptions/ErrorMessage.java
+++
@@ -1,9 +0,0 @@
-package kamysh.exceptions;
-
-public class ErrorMessage {
-    public static final String IS_INTEGER = "Need to be integer";
-    public static final String NOT_FOUND = "Entry not found";
-    public static final String STORAGE_SERVICE_REQUEST_FAILED = "Storage service request failed";
-    public static final String REMOTE_SERVICE_UNAVAILABLE = "Remote Service is not available";
-
-}
